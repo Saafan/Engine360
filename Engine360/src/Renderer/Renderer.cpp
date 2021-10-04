c579892e0dba213fce039e535ca7987e2c172c67
Abdullah Saafan <51491838+Saafan@users.noreply.github.com>
2021-10-04 10:02:02 +0200

    Revert "Uniform Class #3"
    This reverts commit 1e74be4fd0798a644c7d4f3d3c420d4f29e2cea2.

--- Engine360/src/Renderer/Renderer.cpp
+++ Engine360/src/Renderer/Renderer.cpp
@@ -12,14 +12,14 @@ void Renderer::SetShader(Shader* shader)
 
 Renderer::Renderer()
 {
-	curCameraPos = new glm::vec3(0.0f, 0.5f, 1.5f);
+	*curCameraPos = glm::vec3(0.0f, 0.5f, 1.5f);
 }
 
 void Renderer::RenderModels()
 {
-	//for (auto& uniform : uniforms)
-	//	if (uniform->isChanged)
-	//		uniform->UpdateShaderUniform();
+	for (const auto& uniform : uniforms)
+		if (uniform.isChanged)
+			uniform.UpdateShaderUniform();
 	for (const auto& model : models)
 		if (model->visible)
 			model->Render();
--- Engine360/src/Uniform.h
+++ Engine360/src/Uniform.h
@@ -10,8 +10,6 @@ class UniformBase
 {
 public:
 	UniformBase(const char* name, void (*setShaderUniformFunc)());
-	UniformBase(const char* name);
-	UniformBase() = default;
 	unsigned int GetUniformLocation();
 	void UpdateShaderUniform();
 	bool isChanged = true;
@@ -21,7 +19,7 @@ protected:
 	unsigned int uniformLocation = 0;
 	Shader* shader = nullptr;
 	const char* name = "NULL";
-	void (*setShaderUniformFunc)() = nullptr;
+	void (*setShaderUniformFunc)();
 };
 
 
@@ -43,7 +41,6 @@ private:
 
 
 template <typename type>
-inline
 void Uniform<type>::UpdateShaderUniform()
 {
 	if (!isChanged && data != oldData)
@@ -56,42 +53,36 @@ void Uniform<type>::UpdateShaderUniform()
 }
 
 template <>
-inline
 void Uniform<int>::UpdateShaderUniformHelper()
 {
 	glUniform1i(uniformLocation, data);
 }
 
 template <>
-inline
 void Uniform<unsigned int>::UpdateShaderUniformHelper()
 {
 	glUniform1ui(uniformLocation, data);
 }
 
 template <>
-inline
 void Uniform<float>::UpdateShaderUniformHelper()
 {
 	glUniform1f(uniformLocation, data);
 }
 
 template <>
-inline
 void Uniform<glm::vec3>::UpdateShaderUniformHelper()
 {
 	glUniform3f(uniformLocation, data.x, data.y, data.z);
 }
 
 template <>
-inline
 void Uniform<glm::vec4>::UpdateShaderUniformHelper()
 {
 	glUniform4f(uniformLocation, data.x, data.y, data.z, data.w);
 }
 
 template <>
-inline
 void Uniform<glm::mat4>::UpdateShaderUniformHelper()
 {
 	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(data));
@@ -101,22 +92,24 @@ template <typename type>
 inline
 Uniform<type>::Uniform(const char* name, type* dataPtr, Shader* shader, bool isStatic)
 {
+	UniformBase uniformFunctionPtr(name, &Uniform::UpdateShaderUniformHelper);
+	if (!isStatic)
+		Renderer::Get().uniforms.push_back(UniformBase(uniformFunctionPtr));
+	data = *dataPtr;
 	this->shader = shader;
 	this->isStatic = isStatic;
-	setShaderUniformFunc = &Uniform::UpdateShaderUniformHelper;
-	if (!isStatic)
-		Renderer::Get().uniforms.push_back(this);
-	data = *dataPtr;
 }
 
 template <typename type>
-Uniform<type>::Uniform(const char* name, type data, Shader* shader, bool isStatic)
+inline
+Uniform<type>::Uniform(const char* name, type data, Shader* shader, bool isStatic /*= false*/)
 {
+	UniformBase uniformFunctionPtr(name, &Uniform::UpdateShaderUniformHelper);
+	if (!isStatic)
+		Renderer::Get().uniforms.push_back(UniformBase(uniformFunctionPtr));
+	this->data = data;
 	this->shader = shader;
 	this->isStatic = isStatic;
-	if (!isStatic)
-		Renderer::Get().uniforms.push_back(this);
-	this->data = data;
 }
 
 template <typename type>
